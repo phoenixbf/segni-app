d8e2c6cd5505e969b20336da2289ad6dbc30080d
Bruno Fanini <phoenixbf@gmail.com>
2021-12-14 01:23:06 +0100

    implementation now based on XPF; support for URL parameters (cluster, pano id); per-period proxies; semantic filtering

--- js/old-main.js
+++ js/old-main.js
@@ -15,21 +15,20 @@ window.APP = APP;
 
 APP.FOV_STD = 70.0;
 
-//APP.P_MODERN = 0;
-//APP.P_PAST   = 1;
+APP.P_MODERN = 0;
+APP.P_PAST   = 1;
 
 APP.contentDir = "content/";
 APP.audioDir   = "audio/";
 
 APP.conf = undefined;
 
-APP.argConf = undefined;
-APP.argPano = undefined;
-
-APP.currperiod = "m";
+APP.currperiod = APP.P_MODERN;
+APP.currpano   = 0;
+APP.nextpano   = -1;
 
 APP._bFirstPOV = false;
-APP._rotOffset = new THREE.Vector3(0.0, -(Math.PI * 0.5), 0.0);
+APP._pRotOffset = -(Math.PI * 0.5);
 
 APP._bShowingPanel = false;
 APP.auGen = undefined;
@@ -65,8 +64,12 @@ APP.init = ()=>{
     $("#idSlider").on("input change",()=>{
         let v = parseInt( $("#idSlider").val() );
 
-        if (v === 0) APP.switchToPeriod("m");
-        else APP.switchToPeriod("a");
+        if (APP.currperiod !== v){
+            console.log("Switching to: "+v);
+
+            APP.currperiod = v;
+            APP.loadPanorama();
+        }
     });
 
     //$("#idPanel").hide();
@@ -81,47 +84,137 @@ APP.init = ()=>{
     APP._matSemHL.uniforms.opacity.value = 0.4;
 
     // Load config
-    APP.argConf = ATON.FE.urlParams.get('c');
-    if (APP.argConf) APP.loadConfig("config-"+ APP.argConf + ".json");
-
-    if (ATON.FE.urlParams.get('p') !== null){
-        APP.argPano = parseInt( ATON.FE.urlParams.get('p') );
-        console.log("Initial pano: " + APP.argPano);
-    }
+    APP.loadConfig("config.json");
 
     // update routine
-    //ATON.addUpdateRoutine( APP.update );
+    ATON.addUpdateRoutine( APP.update );
 
-
-    // XPF
-    let rf = ATON.device.isMobile? "-4k-" : "-8k-";
-
-    ATON.XPFNetwork.setPathModifier((path)=>{
-        console.log(path)
-        let p = path + rf + APP.currperiod + ".jpg";
-        return p;
-    });
+    APP._dirNode = new THREE.Vector3();
+    APP._posNode = new THREE.Vector3();
 };
 
-APP.switchToPeriod = (pname)=>{
-    if (pname === APP.currperiod) return;
 
-    APP.currperiod = pname;
-    ATON.XPFNetwork.updateCurrentXPFbaseLayer();
+APP.handleNextPanoSelection = ()=>{
+    if (APP.conf === undefined) return;
+    if (APP.conf.network === undefined) return;
 
-    APP.filterSemantics();
+    if (ATON.Nav.isTransitioning()) return;
+
+    let E = ATON.Nav.getCurrentEyeLocation();
+    let V = ATON.Nav.getCurrentDirection();
+
+    //console.log(E);
+
+    // Selected next loc
+    APP.nextpano = -1;
+    let mindist  = undefined;
+
+    for (let i=0; i<APP.conf.network.length; i++){
+        if (i !== APP.currpano){
+            let p = APP.conf.network[i];
+
+            APP._posNode.set(p.pos[0], p.pos[1], p.pos[2]);
+
+            APP._dirNode.x = APP._posNode.x - E.x;
+            APP._dirNode.y = APP._posNode.y - E.y;
+            APP._dirNode.z = APP._posNode.z - E.z;
+            APP._dirNode = APP._dirNode.normalize();
+
+            let v = APP._dirNode.dot(V);
+            if (v > 0.8){
+                let d = E.distanceToSquared(APP._posNode);
+
+                if (mindist === undefined || d<mindist){
+                    mindist      = d;
+                    APP.nextpano = i;
+                    //console.log(APP.nextpano);
+                }
+            }
+        }
+    }
+
+    if (APP.nextpano < 0){
+        APP.suiTelep.visible = false;
+        return;
+    }
+
+    const NL = APP.conf.network[APP.nextpano];
+
+    APP.suiTelep.position.x = NL.pos[0];
+    APP.suiTelep.position.y = NL.pos[1]; // - 1.3;
+    APP.suiTelep.position.z = NL.pos[2];
+/*
+    APP.suiTelep.setPosition(
+        E.x + (APP._dirNode.x * ATON.Nav.STD_FAR * 0.1),
+        E.y + (APP._dirNode.y * ATON.Nav.STD_FAR * 0.1),
+        E.z + (APP._dirNode.z * ATON.Nav.STD_FAR * 0.1)
+    );
+*/
+    APP.suiTelep.visible = true;
+
+    //console.log(APP.suiTelep.position);
 };
 
 // Update
 APP.update = ()=>{
-    //APP.handleNextPanoSelection();
+    APP.handleNextPanoSelection();
 
     //if (ATON._queryDataSem) console.log(ATON._queryDataSem);
     //if (ATON._queryDataUI)  console.log(ATON._queryDataUI);
 };
 
+APP.loadPanorama = ()=>{
+    if (APP.conf.network === undefined) return;
+
+    let p = APP.conf.network[APP.currpano];
+    let rf = ATON.device.isMobile? "4k/" : "8k/";
+
+    let post = (APP.currperiod === APP.P_MODERN)? "m.jpg" : "a.jpg";
+    let purl = rf + p.name + post;
+
+    ATON.setMainPanorama(purl);
+    ATON.setMainPanoramaRotation(APP._pRotOffset);
+
+    //console.log(purl);
+};
+
+APP.moveTo = (i)=>{
+    if (APP.conf.network === undefined) return;
+
+    APP.currpano = i;
+    APP.loadPanorama();
+
+    let p = APP.conf.network[APP.currpano];
+    let pos = p.pos;
+    console.log("Moving to ",pos);
+    console.log(p.name);
+
+    let V = ATON.Nav.getCurrentDirection();
+
+    let P = new ATON.POV();
+    P.setPosition(pos[0],pos[1],pos[2]);
+
+    if (APP._bFirstPOV) 
+        P.setTarget(pos[0]+V.x, pos[1]+V.y, pos[2]+V.z);
+    else 
+        P.setTarget(pos[0]+1.0, pos[1], pos[2]);
+    
+    P.setFOV(APP.FOV_STD);
+
+    ATON.Nav.requestPOV(P, 0.5);
+    APP._bFirstPOV = true;
+
+    //ATON.AudioHub.playOnceGlobally(APP.audioDir + "teleport.wav");
+
+    //ATON.setMainPanoramaLocation( new THREE.Vector3(pos[0],pos[1],pos[2]));
+
+    //console.log(APP.currpano);
+
+    APP.filterSemantics();
+};
+
 APP.filterSemantics = ()=>{
-    let p = APP.conf.network[ ATON.XPFNetwork.getCurrentXPFindex() ];
+    let p = APP.conf.network[APP.currpano];
     if (p === undefined) return;
 
     if (p.semlist === undefined){
@@ -129,19 +222,9 @@ APP.filterSemantics = ()=>{
         return;
     }
 
-    // Clear all
     for (let j in APP._semlist) ATON.getSemanticNode(APP._semlist[j]).hide();
 
-    let proxiesList = p.semlist[APP.currperiod];
-    console.log(proxiesList)
-
-    if (proxiesList !== undefined){
-        for (let j in proxiesList){
-            let S = ATON.getSemanticNode(proxiesList[j]);
-            if (S !== undefined) S.show();
-            console.log(S)
-        }
-    }
+    for (let j in p.semlist) ATON.getSemanticNode(p.semlist[j]).show();
 };
 
 // Load config
@@ -152,21 +235,13 @@ APP.loadConfig = (path)=>{
 
         APP.conf = data;
 
-        if (data.network){
-            for (let i in data.network){
-                let d = data.network[i];
-
-                let xpf = new ATON.XPF();
-
-                xpf.setBaseLayer(d.name);
-                xpf.setLocation(d.pos[0],d.pos[1],d.pos[2]);
-                if (d.rot !== undefined) xpf.setRotation( d.rot[0],d.rot[1],d.rot[2] );
-
-                ATON.XPFNetwork.add( xpf );
-            }
-        }
+        //let h = APP.conf.network[0];
+        //ATON.Nav.setHomePOV( new ATON.POV().setPosition(h.pos[0],h.pos[1],h.pos[2]).setTarget(h.pos[0],h.pos[1],h.pos[2]-1.0).setFOV(APP.FOV_STD) );
 
         ATON.createSemanticNode("mainsem").attachToRoot();
+        //ATON.createSemanticNode("esterni").load("proxies/Esterno.glb").attachTo("mainsem");
+
+        //ATON.createSceneNode("esterni").load("proxies/Esterno.glb").attachToRoot();
 
         APP._semlist = [];
 
@@ -196,8 +271,6 @@ APP.loadConfig = (path)=>{
             ATON._bqSem = true;
             //ATON._bQuerySemOcclusion = true;
             //ATON._bqScene = true;
-
-            console.log(APP._semlist)
             
             //console.log(ATON._rootSem);
         }
@@ -279,8 +352,7 @@ APP.setupEvents = ()=>{
 
         //APP.popupStart();
 
-        if (APP.argPano !== undefined) ATON.XPFNetwork.requestTransitionByIndex(APP.argPano);
-        else ATON.XPFNetwork.requestTransitionByIndex(0);
+        APP.moveTo(0); // 0
     });
 
     ATON.on("AllNodeRequestsCompleted", ()=>{
@@ -312,31 +384,6 @@ APP.setupEvents = ()=>{
         APP.updatePanel(ATON._hoveredSemNode);
     });
 
-
-    // We subscribe to next XPF detected
-    ATON.on("NextXPF", i =>{
-        // No next XPF detected, hide the indicator 
-        if (i === undefined){
-            APP.suiTelep.visible = false;
-            return;
-        }
-        // Next XPF detected, show indicator and update its location
-        let xpf = ATON.XPFNetwork.getXPFbyIndex(i);
-
-        APP.suiTelep.visible = true;
-        APP.suiTelep.position.copy( xpf.getLocation() );
-    });
-
-    ATON.on("CurrentXPF", i => {
-        //console.log(i)
-        APP.filterSemantics();
-    });
-
-    // When we request a locomotion node transition, hide indicator
-    ATON.on("LocomotionNodeRequested", locnode => {
-        APP.suiTelep.visible = false;
-    });
-
 /*
     ATON.on("UINodeLeave", (uiid)=>{
         console.log(uiid);
@@ -345,20 +392,20 @@ APP.setupEvents = ()=>{
         console.log(uiid);
     });
 */
-
-/*
     ATON.on("DoubleTap", (e)=>{
         if (APP.nextpano < 0) return;
 
         APP.moveTo(APP.nextpano);
     });
-*/
+
     ATON.on("KeyPress", (k)=>{
         if (k==='p'){
-            APP.switchToPeriod("a");
+            APP.currperiod = APP.P_PAST;
+            APP.loadPanorama();
         }
         if (k==='m'){
-            APP.switchToPeriod("m");
+            APP.currperiod = APP.P_MODERN;
+            APP.loadPanorama();
         }
 
         if (k==='e'){
@@ -388,23 +435,6 @@ APP.buildSlider = (id)=>{
 
 
 APP.buildSUI = ()=>{
-
-    APP.matTelep = new THREE.SpriteMaterial({ 
-        map: new THREE.TextureLoader().load( APP.contentDir+"ui/teleport.png" ), 
-        transparent: true,
-        opacity: 1.0,
-        //depthWrite: false, 
-        depthTest: false
-    });
-
-    //APP.matTelep.sizeAttenuation = false;
-
-    APP.suiTelep = new THREE.Sprite( APP.matTelep );
-    APP.suiTelep.scale.set(0.5,0.5,0.5);
-
-    ATON.getRootUI().add( APP.suiTelep );
-
-/*
     ATON.SUI.bShowInfo = false;
 
     let telSize = 0.1; //1.5;
@@ -422,7 +452,7 @@ APP.buildSUI = ()=>{
     APP.suiTelep = new THREE.Sprite( APP.matTelep );
     APP.suiTelep.scale.set(telSize,telSize,1);
     ATON.getRootUI().add(APP.suiTelep);
-*/
+
 /*
     APP.suiTelep = ATON.SUI.buildPanelNode("suiTeleport", APP.contentDir+"ui/teleport.png", telSize,telSize);
     APP.suiTelep
