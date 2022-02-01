c7a43adf9fe39de2006181d83f1ee0a305f2023b
Bruno Fanini <phoenixbf@gmail.com>
2022-02-01 11:51:26 +0100

    EPSG proj

--- js/home.js
+++ js/home.js
@@ -160,7 +160,10 @@ window.onload = ()=>{
 
     loadConfigs();
 
-    proj4.defs('EPSG:3004', '+proj=tmerc +lat_0=0 +lon_0=15 +k=0.999600 +x_0=2520000 +y_0=0 +ellps=intl +units=m +no_defs');
+    proj4.defs('EPSG:3004', "+proj=tmerc +lat_0=0 +lon_0=15 +k=0.9996 +x_0=2520000 +y_0=0 +ellps=intl +towgs84=-104.1,-49.1,-9.9,0.971,-2.917,0.714,-11.68 +units=m +no_defs");
+    //proj4.defs('EPSG:3004', "+proj=tmerc +lat_0=0 +lon_0=15 +k=0.9996 +x_0=2520000 +y_0=0 +ellps=intl +units=m +no_defs");
+    //proj4.defs('EPSG:3004', '+proj=tmerc +lat_0=0 +lon_0=15 +k=0.999600 +x_0=2520000 +y_0=0 +ellps=intl +units=m +no_defs');
+    
     //console.log(ol.proj)
     ol.proj.proj4.register(proj4);
 
